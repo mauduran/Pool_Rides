b96656bcdd7baad29460506b85db7c0444c6864d
Edgar Medina <is715468@iteso.mx>
2021-02-25 13:04:54 -0600

    Added SignUp2 route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:Pool_Rides/homepage/HomePage.dart';
 import 'package:Pool_Rides/principal/principal.dart';
 import 'package:Pool_Rides/session/sign_up.dart';
+import 'package:Pool_Rides/session/sign_up_2.dart';
 import 'package:Pool_Rides/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -17,9 +18,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/test': (context) => HomePage(title: 'Pool Rides'),
         '/': (context) => Principal(),
+        '/HomePage': (context) => HomePage(title: 'Pool Rides'),
         '/SignUp': (context) => SignUp(),
+        '/SignUp2': (context) => SignUp2(),
       },
     );
   }
