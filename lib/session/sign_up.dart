93589d8e751f323f9a8868d932bd619a14c5b0a5
Edgar Medina <is715468@iteso.mx>
2021-02-25 00:16:38 -0600

    Created SignUp view

---
+++ lib/session/sign_up.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  SignUp({Key key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  DateTime _date = DateTime.now();
+
+  void _selectDate() async {
+    final DateTime newDate = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(1900, 1),
+      lastDate: DateTime(2050, 7),
+      helpText: 'Select a date',
+    );
+
+    if (newDate != null) {
+      setState(() {
+        _date = newDate;
+      });
+    }
+  }
+
+  var _nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Registro",
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "¿Cuál es tu email?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, right: 30, top: 20),
+                    child: TextField(
+                      // cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        // errorText: 'Error message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        // enabledBorder: InputBorder()
+                        // fillColor: Colors.grey[200],
+                        // filled: true,
+                        suffixIcon: Icon(
+                          Icons.mail,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "¿Cómo te llamas?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, right: 30, top: 20),
+                    child: TextField(
+                      // cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        labelText: 'Nombre(s)',
+                        // errorText: 'Error message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        // enabledBorder: InputBorder()
+                        // fillColor: Colors.grey[200],
+                        // filled: true,
+                        // suffixIcon: Icon(
+                        //   Icons.mail,
+                        //   color: Colors.black,
+                        // ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, right: 30, top: 25),
+                    child: TextField(
+                      // cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        labelText: 'Apellido(s)',
+                        // errorText: 'Error message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        // enabledBorder: InputBorder()
+                        // fillColor: Colors.grey[200],
+                        // filled: true,
+                        // suffixIcon: Icon(
+                        //   Icons.mail,
+                        //   color: Colors.black,
+                        // ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "¿Cuál tu fecha de nacimiento?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: _selectDate,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('Selecciona tu fecha'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(Icons.date_range_outlined)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Teléfono",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, right: 30, top: 20),
+                    child: TextField(
+                      // cursorColor: Colors.red,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        labelText: 'Número móvil',
+                        // errorText: 'Error message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        // enabledBorder: InputBorder()
+                        // fillColor: Colors.grey[200],
+                        // filled: true,
+                        suffixIcon: Icon(
+                          Icons.phone,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    right: 12.0,
+                  ),
+                  child: FloatingActionButton(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    foregroundColor: Colors.white,
+                    onPressed: () {
+                      // Respond to button press
+                    },
+                    child: Icon(
+                      Icons.arrow_forward,
+                      size: 37,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
