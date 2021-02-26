159e6d67030699c12d9b4806df7d63f42466a7fa
Edgar Medina <is715468@iteso.mx>
2021-02-25 18:38:28 -0600

    Added Sign In view

---
+++ lib/session/sign_in.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  SignIn({Key key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  var _emailController = TextEditingController();
+  var _passwordController = TextEditingController();
+
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Registro",
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "¿Cuál es tu email?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, right: 30, top: 20),
+                    child: TextField(
+                      controller: _emailController,
+                      // cursorColor: Colors.red,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        // errorText: 'Error message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        // enabledBorder: InputBorder()
+                        // fillColor: Colors.grey[200],
+                        // filled: true,
+                        suffixIcon: Icon(
+                          Icons.mail,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Necesitas una contraseña",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.5,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 30.0, right: 30, top: 20),
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        // errorText: 'Error message',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.remove_red_eye),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30.0),
+                        child: InkWell(
+                          child: new Text(
+                            'Contraseña olvidada',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).pushNamed("/");
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              // TODO: Hacer que el botón aparezca cuando los campos son llenados.
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    right: 12.0,
+                  ),
+                  child: FloatingActionButton(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    foregroundColor: Colors.white,
+                    onPressed: () {
+                      // Respond to button press
+                      Navigator.of(context).pushNamed(
+                        '/HomePage',
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_forward,
+                      size: 37,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
