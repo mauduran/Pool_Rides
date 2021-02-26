6a5298338d290910a48d5eee22fb657156eb27a4
Edgar Medina <is715468@iteso.mx>
2021-02-25 19:54:03 -0600

    Changed principal view

--- lib/principal/principal.dart
+++ lib/principal/principal.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class Principal extends StatefulWidget {
   Principal({Key key}) : super(key: key);
@@ -13,90 +11,60 @@ class _PrincipalState extends State<Principal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: Text("Pool Rides"),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              "assets/images/principal_image.png",
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Es tu viaje.",
-              style: Theme.of(context).textTheme.headline5,
-            ),
-            SizedBox(
-              height: 12.5,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 20,
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: MaterialButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(100.0)),
-                        height: 37.5,
-                        color: Color.fromARGB(255, 51, 174, 250),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Regístrate",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ],
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pushNamed("/PrincipalSignUp");
-                        },
-                      ),
-                    ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/principal_image.png",
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Es tu viaje.",
+            style: Theme.of(context).textTheme.headline5,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 12.5,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  child: new Text(
-                    'Inciar Sesión',
-                    style: TextStyle(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100.0)),
+                      height: 50,
                       color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Ingresa",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed("/PrincipalSignIn");
+                      },
                     ),
                   ),
-                  onTap: () {
-                    Navigator.of(context).pushNamed("/PrincipalSignIn");
-                  },
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
