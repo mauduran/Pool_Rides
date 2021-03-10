a2fcf049f701ca808548b90dde415e990cad9da3
Edgar Medina <is715468@iteso.mx>
2021-03-09 20:10:48 -0600

    Chagend travel model

--- lib/models/travel.dart
+++ lib/models/travel.dart
@@ -1,22 +1,30 @@
+import 'package:Pool_Rides/models/user.dart';
 import 'package:flutter/foundation.dart';
 
 class Travel {
-  final String driverName;
   final String startTime;
-  final String arrivalTime;
-  final double priceTravel;
-  final double stars;
   final String sourceName;
+  final String sourceLocation;
+
+  final String arrivalTime;
   final String destinyName;
-  final String imageDriver;
+  final String destinyLocation;
+
+  final double priceTravel;
+  final DateTime departureDate;
+
+  final User driver;
+  final List<User> passengers;
   Travel({
-    @required this.driverName,
+    @required this.driver,
+    @required this.passengers,
+    @required this.sourceLocation,
+    @required this.destinyLocation,
+    @required this.departureDate,
     @required this.startTime,
     @required this.arrivalTime,
     @required this.priceTravel,
-    @required this.stars,
     @required this.sourceName,
     @required this.destinyName,
-    @required this.imageDriver,
   });
 }
