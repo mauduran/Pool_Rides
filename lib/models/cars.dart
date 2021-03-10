7a1ec9c0d275d79674aa99d4f76287d33a4210d8
Edgar Medina <is715468@iteso.mx>
2021-03-10 17:06:04 -0600

    Added cars model

---
+++ lib/models/cars.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class Cars {
+  String model;
+  String color;
+  String image;
+  Cars({
+    @required this.model,
+    @required this.color,
+    @required this.image,
+  });
+}
