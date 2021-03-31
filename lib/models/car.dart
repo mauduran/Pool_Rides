5463001231c5955cdb86c21a89ebcac372eee80a
Edgar Medina <is715468@iteso.mx>
2021-03-31 14:30:35 -0600

    added new attributes to car model

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/foundation.dart';
 
 class Car {
+  String brand;
   String model;
+  String plate;
   String color;
+  int year;
   String image;
   Car({
+    this.brand,
     @required this.model,
+    this.plate,
     @required this.color,
+    this.year,
     @required this.image,
   });
 }
