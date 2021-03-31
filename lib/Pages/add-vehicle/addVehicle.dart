b55928fea20b25822640301ad3e51e109a8cb75f
Edgar Medina <is715468@iteso.mx>
2021-03-31 14:31:08 -0600

    added new widgets and a new class for dropdown menus

--- lib/Pages/add-vehicle/addVehicle.dart
+++ lib/Pages/add-vehicle/addVehicle.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pool_rides/utils/lists.dart';
+import 'package:intl/intl.dart';
 
 class AddVehiclePage extends StatefulWidget {
   AddVehiclePage({Key key}) : super(key: key);
@@ -12,8 +12,70 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
   var _marcaController = TextEditingController();
   var _modeloController = TextEditingController();
   var _placasController = TextEditingController();
-  var _colorController = TextEditingController();
-  var _yearController = TextEditingController();
+  // var _colorController = TextEditingController();
+  // var _yearController = TextEditingController();
+
+  List<DropdownMenuItem<ListItem>> _dropdownColors;
+  ListItem _colorSelected;
+
+  List<DropdownMenuItem<ListItem>> _dropdownYears;
+  ListItem _yearSelected;
+
+  List<DropdownMenuItem<ListItem>> buildColorDropdown() {
+    List<DropdownMenuItem<ListItem>> items = [];
+    List<String> colors = [
+      "Blanco",
+      "Negro",
+      "Gris",
+      "Plata",
+      "Azul",
+      "Beige",
+      "Rojo",
+      "Verde"
+    ];
+
+    for (int i = 0; i < colors.length; i++) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(colors[i]),
+          value: ListItem(
+            name: colors[i],
+            value: i,
+          ),
+        ),
+      );
+    }
+    return items;
+  }
+
+  List<DropdownMenuItem<ListItem>> buildYearDropdown() {
+    List<DropdownMenuItem<ListItem>> items = [];
+
+    String _currentYearStr = DateFormat('y').format(DateTime.now());
+    int _currentYear = int.parse(_currentYearStr);
+
+    for (int i = _currentYear - 6, j = 0; i < _currentYear + 1; i++, j++) {
+      items.add(
+        DropdownMenuItem(
+          child: Text("$i"),
+          value: ListItem(
+            name: "$i",
+            value: j,
+          ),
+        ),
+      );
+    }
+    return items;
+  }
+
+  void initState() {
+    super.initState();
+    _dropdownColors = buildColorDropdown();
+    _colorSelected = _dropdownColors[0].value;
+
+    _dropdownYears = buildYearDropdown();
+    _yearSelected = _dropdownYears[_dropdownYears.length - 1].value;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +87,11 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
             Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 25.0,
-                vertical: 5,
+                vertical: 10,
               ),
               child: Text(
                 "¿Cuál es la marca de tu vehículo?",
-                style: Theme.of(context).textTheme.headline4,
+                style: Theme.of(context).textTheme.headline5,
               ),
             ),
             Padding(
@@ -59,7 +121,7 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
               ),
               child: Text(
                 "¿Cuál es el modelo?",
-                style: Theme.of(context).textTheme.headline4,
+                style: Theme.of(context).textTheme.headline5,
               ),
             ),
             Padding(
@@ -89,7 +151,7 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
               ),
               child: Text(
                 "Ingresa las placas de tu vehiculo",
-                style: Theme.of(context).textTheme.headline4,
+                style: Theme.of(context).textTheme.headline5,
               ),
             ),
             Padding(
@@ -110,7 +172,7 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
               ),
             ),
             SizedBox(
-              height: 15,
+              height: 10,
             ),
             Row(
               mainAxisSize: MainAxisSize.max,
@@ -130,22 +192,37 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
                           style: Theme.of(context).textTheme.headline6,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 20.0,
-                          right: 20.0,
-                          top: 10,
-                        ),
-                        child: TextField(
-                          controller: _colorController,
-                          decoration: InputDecoration(
-                            labelText: 'Color',
-                            hintText: "Ejemplo: Blanco",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                left: 20.0,
+                                right: 20.0,
+                                top: 10,
+                              ),
+                              child:
+                                  // TextField(
+                                  //   controller: _colorController,
+                                  //   decoration: InputDecoration(
+                                  //     labelText: 'Color',
+                                  //     hintText: "Ejemplo: Blanco",
+                                  //     border: OutlineInputBorder(
+                                  //       borderRadius: BorderRadius.circular(50),
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                  DropdownButton<ListItem>(
+                                items: _dropdownColors,
+                                value: _colorSelected,
+                                onChanged: (newValue) {
+                                  _colorSelected = newValue;
+                                  setState(() {});
+                                },
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ],
                   ),
@@ -165,28 +242,191 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
                           style: Theme.of(context).textTheme.headline6,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 20.0,
-                          right: 20.0,
-                          top: 10,
-                        ),
-                        child: TextField(
-                          controller: _yearController,
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            labelText: 'Año',
-                            hintText: "Ejemplo: 2019",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(50),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                left: 20.0,
+                                right: 20.0,
+                                top: 10,
+                              ),
+                              child:
+                                  // TextField(
+                                  //   controller: _yearController,
+                                  //   keyboardType: TextInputType.number,
+                                  //   decoration: InputDecoration(
+                                  //     labelText: 'Año',
+                                  //     hintText: "Ejemplo: 2019",
+                                  //     border: OutlineInputBorder(
+                                  //       borderRadius: BorderRadius.circular(50),
+                                  //     ),
+                                  //   ),
+                                  // ),
+                                  DropdownButton<ListItem>(
+                                items: _dropdownYears,
+                                value: _yearSelected,
+                                onChanged: (newValue) {
+                                  _yearSelected = newValue;
+                                  setState(() {});
+                                },
+                                // iconSize: 0,
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
                     ],
                   ),
                 ),
               ],
+            ),
+            GestureDetector(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 30.0,
+                  top: 20,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.car_rental,
+                      size: 25,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "Añadir foto de tu coche",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('Imagen de tu coche'),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Divider(
+                              thickness: 1.5,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ],
+                        ),
+                        titlePadding: EdgeInsets.only(
+                          top: 20.0,
+                        ),
+                        content: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: MaterialButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(100.0)),
+                                  height: 50,
+                                  color: Color.fromARGB(255, 51, 174, 250),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Icon(
+                                        Icons.camera_alt,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "¡Tomale una foto!",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 17.5,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  onPressed: () {
+                                    //bloc
+                                    // _userBloc
+                                    //     .add(ChangeAccountImageByPhotoEvent());
+                                  },
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: MaterialButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(100.0)),
+                                  height: 50,
+                                  color: Theme.of(context).primaryColor,
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: [
+                                      Icon(
+                                        Icons.camera,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text(
+                                        "Selecciona una foto",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 17.5,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  onPressed: () {
+                                    //_userBloc
+                                    // _userBloc.add(
+                                    //     ChangeAccountImageByGalleryEvent());
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text(
+                              "Aceptar",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    });
+              },
             ),
             SizedBox(
               width: double.infinity,
@@ -210,4 +450,14 @@ class _AddVehiclePageState extends State<AddVehiclePage> {
       ),
     );
   }
+}
+
+class ListItem {
+  final int value;
+  final String name;
+
+  ListItem({
+    @required this.value,
+    @required this.name,
+  });
 }
