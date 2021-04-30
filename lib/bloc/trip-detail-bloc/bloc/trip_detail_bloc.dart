3c833dc011e3b5a687fe188bb4afec18d75562f0
Edgar Medina <is715468@iteso.mx>
2021-04-30 12:49:44 -0500

    added my Trip service to add a user to trip

--- lib/bloc/trip-detail-bloc/bloc/trip_detail_bloc.dart
+++ lib/bloc/trip-detail-bloc/bloc/trip_detail_bloc.dart
@@ -5,10 +5,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:pool_rides/models/my-trip.dart';
 import 'package:pool_rides/models/trip.dart';
 import 'package:pool_rides/models/user.dart';
 import 'package:pool_rides/services/auth-service.dart';
 import 'package:pool_rides/services/user-service.dart';
+import 'package:pool_rides/services/my-trip-service.dart';
 
 part 'trip_detail_event.dart';
 part 'trip_detail_state.dart';
@@ -17,6 +19,7 @@ class TripDetailBloc extends Bloc<TripDetailEvent, TripDetailState> {
   TripDetailBloc() : super(TripDetailInitial());
   UserAuthProvider _authProvider = UserAuthProvider();
   static final UserService _userService = UserService();
+  static final MyTripService _myTripService = MyTripService();
 
   CollectionReference trips = FirebaseFirestore.instance.collection('trips');
   CollectionReference users = FirebaseFirestore.instance.collection('users');
@@ -25,21 +28,28 @@ class TripDetailBloc extends Bloc<TripDetailEvent, TripDetailState> {
   Stream<TripDetailState> mapEventToState(
     TripDetailEvent event,
   ) async* {
-    // TODO: implement mapEventToState
     if (event is AddUserToTripEvent) {
       try {
         yield LoadingState();
         var connectivityResult = await (Connectivity().checkConnectivity());
         if (connectivityResult == ConnectivityResult.mobile ||
             connectivityResult == ConnectivityResult.wifi) {
-          bool isAdded = await addUserToTrip(event.trip);
+          bool isAdded = await addUserToTripBloc(
+            trip: event.trip,
+            distOrigin: event.distanceOrigin,
+            distDest: event.distanceDestination,
+          );
 
           if (isAdded) {
             DocumentSnapshot updatedTrip =
                 await trips.doc(event.trip.tripId).get();
-            Trip newTrip = Trip.fromJson(updatedTrip
-                .data()); //Truena aquí. Pero ya agrega el usuario al trip y en el usuario crea la colección de mis trips y agrega el viaje.
-            yield UserAddedSuccesfully(trip: newTrip);
+            Trip newTrip = Trip.fromJson(updatedTrip.data());
+            yield UserAddedSuccesfully(
+              trip: newTrip,
+              distanceOrigin: event.distanceOrigin,
+              distanceDestination: event.distanceDestination,
+            );
+            return;
           }
         }
         throw "Error";
@@ -52,21 +62,26 @@ class TripDetailBloc extends Bloc<TripDetailEvent, TripDetailState> {
     }
   }
 
-  addUserToTrip(Trip trip) async {
+  addUserToTripBloc({
+    Trip trip,
+    double distOrigin,
+    double distDest,
+  }) async {
     try {
       User currentUser =
           await _userService.getCurrentUser(_authProvider.getUid());
-      print("despues de current userr");
-      await trips.doc(trip.tripId).update({
-        "passengers": FieldValue.arrayUnion([currentUser.toMap()])
-      });
-      print("despues de agregar el pasajero al viaje");
+
+      MyTrip newTrip = await _myTripService.addUserToTrip(
+          user: currentUser,
+          trip: trip,
+          distanceOrigin: distOrigin,
+          distanceDestination: distDest);
+
       await users
           .doc(_authProvider.getUid())
           .collection("myTrips")
           .doc(trip.tripId)
-          .set(trip.toMap()); // Modelo my trip
-      print("despues de agregar el viaje a my trips");
+          .set(newTrip.toMap());
       return true;
     } catch (e) {
       print("Error: $e");
